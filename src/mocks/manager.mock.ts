4002052a3f6c08a766c343afff896e1b5fa46b6a
Eric Kreis <erickreis793@gmail.com>
2023-04-10 12:26:24 -0300

    create: prisma manager mock

--- src/mocks/manager.mock.ts
+++ src/mocks/manager.mock.ts
@@ -1,9 +1,15 @@
 import { Manager } from '@domain/manager/manager';
 import { faker } from '@faker-js/faker';
+import { Manager as PrismaManager } from '@prisma/client';
 
 export const managerMock: Manager = {
   id: faker.database.mongodbObjectId(),
   createdAt: new Date(),
   updatedAt: new Date(),
   email: faker.internet.email(),
+};
+
+export const prismaManagerMock: PrismaManager = {
+  ...managerMock,
+  password: faker.internet.password(),
 };
