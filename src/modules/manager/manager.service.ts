0ce693671cab0659cee1b169f70a86d257113e68
Eric Kreis <erickreis793@gmail.com>
2023-04-10 13:16:33 -0300

    tdd: manager service findByCredentials implementation

--- src/modules/manager/manager.service.ts
+++ src/modules/manager/manager.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ManagerService as DomainManagerService } from '@domain/manager/manager.service';
 import { Manager } from '@domain/manager/manager';
 import { ManagerRepository } from '@domain/manager/manager.repository';
+import { ManagerNotFoundException } from '@domain/manager/exceptions/manager-not-found.exception';
 import { UpdateManagerDto } from './dto/update-manager.dto';
 
 @Injectable()
@@ -13,7 +14,9 @@ export class ManagerService extends DomainManagerService {
   }
 
   public async findByCredentials(email: string, password: string): Promise<Manager> {
-    throw new Error('Method not implemented.');
+    const manager = await this._managerRepository.findByCredentials(email, password);
+    if (!manager) throw new ManagerNotFoundException();
+    return manager;
   }
 
   public async update(id: string, payload: UpdateManagerDto): Promise<Manager> {
