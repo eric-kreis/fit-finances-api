e9679d4c5870708ba208cfd27ab29f4522337f9c
Eric Kreis <erickreis793@gmail.com>
2023-04-10 02:03:25 -0300

    tdd: manager prisma repository create implementation

--- src/infrastructure/repositories/manager/manager.prisma.repositoy.ts
+++ src/infrastructure/repositories/manager/manager.prisma.repositoy.ts
@@ -1,15 +1,31 @@
+import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
 import { Manager } from '@domain/manager/manager';
 import { ManagerRepository } from '@domain/manager/manager.repository';
 import { CreateManagerType } from '@domain/manager/manager.types';
+import { ManagerAlreadyRegistredException } from '@domain/manager/exceptions/manager-already-registred.exception';
 import { PrismaService } from '../../prisma.service';
 
+@Injectable()
 export class ManagerPrismaRepository extends ManagerRepository {
   constructor(private readonly _prismaService: PrismaService) {
     super();
   }
 
-  public create(data: CreateManagerType): Promise<Manager> {
-    throw new Error('Method not implemented.');
+  public async create({ email, password }: CreateManagerType): Promise<Manager> {
+    const managerWithEmail = await this._prismaService.manager.findUnique({ where: { email } });
+
+    if (managerWithEmail) throw new ManagerAlreadyRegistredException();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newManager = await this._prismaService.manager.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return new Manager(newManager);
   }
 
   public findById(id: string): Promise<Manager> {
