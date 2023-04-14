6ce6467cc77b56b42f91d858a197ee264391fdd7
Eric Kreis <erickreis793@gmail.com>
2023-04-14 18:05:35 -0300

    update: adding auth module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ManagerModule } from '@modules/manager/manager.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
     ManagerModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
