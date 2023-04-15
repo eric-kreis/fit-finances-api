1b379ff48965d6fc884e67dcb53739cc06bebd38
Eric Kreis <erickreis793@gmail.com>
2023-04-15 00:43:33 -0300

    create: member types

---
+++ src/domain/member/member.types.ts
@@ -0,0 +1,16 @@
+import { PaginationType } from '@domain/types';
+import { Member } from './member';
+
+export type CreateMemberType = Omit<Member, 'id' | 'createdAt' | 'updatedAt'>;
+
+export type UpdateMemberType = Partial<CreateMemberType>;
+
+export type MemberPaginationType = PaginationType<Member> & {
+  serach?: string;
+};
+
+export type FindOneMemberType = {
+  id: string;
+  email: string;
+  cpf: string;
+};
