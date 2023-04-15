8a21a38c808bec03221090339fc61e0b1a3a6e2f
Eric Kreis <erickreis793@gmail.com>
2023-04-15 16:30:51 -0300

    tdd: member service create implementation

--- src/modules/member/member.service.ts
+++ src/modules/member/member.service.ts
@@ -1,8 +1,10 @@
 import { Member } from '@domain/member/member';
 import { MemberRepository } from '@domain/member/member.repository';
 import { MemberService as DomainMemberService } from '@domain/member/member.service';
-import { CreateMemberType, MemberPaginationType } from '@domain/member/member.types';
+import { MemberPaginationType } from '@domain/member/member.types';
 import { Injectable } from '@nestjs/common';
+import { CreateMemberDto } from './dto/create-member.dto';
+import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Injectable()
 export class MemberService extends DomainMemberService {
@@ -12,8 +14,8 @@ export class MemberService extends DomainMemberService {
     super();
   }
 
-  public async create(data: CreateMemberType): Promise<Member> {
-    throw new Error('Method not implemented.');
+  public async create(data: CreateMemberDto): Promise<Member> {
+    return this._memberRepository.create(data);
   }
 
   public findMany(query: MemberPaginationType): Promise<Member[]> {
@@ -32,7 +34,7 @@ export class MemberService extends DomainMemberService {
     throw new Error('Method not implemented.');
   }
 
-  public async update(id: string, paylaod: Partial<CreateMemberType>): Promise<Member> {
+  public async update(id: string, paylaod: UpdateMemberDto): Promise<Member> {
     throw new Error('Method not implemented.');
   }
 
