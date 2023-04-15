854808b74279439f8a0eb28bea66ffea6b683f7b
Eric Kreis <erickreis793@gmail.com>
2023-04-15 00:44:26 -0300

    fix: member exceptions imports

--- src/domain/member/exceptions/member-already-registred.exception.ts
+++ src/domain/member/exceptions/member-already-registred.exception.ts
@@ -1,5 +1,5 @@
-import { DomainErrorCode } from '@domain/domain-error-code.enum';
 import { DomainException } from '@domain/domain.exception';
+import { DomainErrorCode } from '@domain/enums';
 
 export class MemberAlreadyRegistredException extends DomainException {
   code = DomainErrorCode.MemberAlreadyRegistred;
