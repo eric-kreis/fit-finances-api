8926e9c1e6c04315cc53d64e128a330708124c77
Eric Kreis <erickreis793@gmail.com>
2023-04-15 01:27:03 -0300

    tdd: member prisma repository create method

--- src/infrastructure/repositories/member/member.prisma.repository.ts
+++ src/infrastructure/repositories/member/member.prisma.repository.ts
@@ -1,8 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { MemberAlreadyRegistredException } from '@domain/member/exceptions/member-already-registred.exception';
 import { Member } from '@domain/member/member';
 import { MemberRepository } from '@domain/member/member.repository';
 import { CreateMemberType, MemberPaginationType, FindOneMemberType } from '@domain/member/member.types';
 import { PrismaService } from '@infrastructure/prisma.service';
 
+@Injectable()
 export class MemberPrismaRepository extends MemberRepository {
   constructor(
     private readonly _prismaService: PrismaService,
@@ -10,23 +13,41 @@ export class MemberPrismaRepository extends MemberRepository {
     super();
   }
 
-  public create(data: CreateMemberType): Promise<Member> {
+  public async create(data: CreateMemberType): Promise<Member> {
+    const memberWithEmail = await this._prismaService.member.findUnique({
+      where: { email: data.email },
+    });
+    if (memberWithEmail) throw new MemberAlreadyRegistredException('Member already registred with this email');
+
+    const memberWithCpf = await this._prismaService.member.findUnique({
+      where: { cpf: data.cpf },
+    });
+    if (memberWithCpf) throw new MemberAlreadyRegistredException('Member already registred with this cpf');
+
+    const newMember = await this._prismaService.member.create({
+      data: {
+        cpf: data.cpf,
+        email: data.email,
+        name: data.name,
+      },
+    });
+
+    return new Member(newMember);
+  }
+
+  public async findMany(query: MemberPaginationType): Promise<Member[]> {
     throw new Error('Method not implemented.');
   }
 
-  public findMany(query: MemberPaginationType): Promise<Member[]> {
+  public async findOne(query: FindOneMemberType): Promise<Member | null> {
     throw new Error('Method not implemented.');
   }
 
-  public findOne(query: FindOneMemberType): Promise<Member | null> {
+  public async update(id: string, payload: Partial<CreateMemberType>): Promise<Member> {
     throw new Error('Method not implemented.');
   }
 
-  public update(id: string, payload: Partial<CreateMemberType>): Promise<Member> {
-    throw new Error('Method not implemented.');
-  }
-
-  public delete(id: string): Promise<Member> {
+  public async delete(id: string): Promise<Member> {
     throw new Error('Method not implemented.');
   }
 }
