cd9507d9516d00666e5c490409e6796f1cd7fd6f
Eric Kreis <erickreis793@gmail.com>
2023-04-14 22:36:45 -0300

    update: reference on mongo id dto docs

--- src/shared/dtos/mongo-id.dto.ts
+++ src/shared/dtos/mongo-id.dto.ts
@@ -3,7 +3,7 @@ import { IsMongoId } from 'class-validator';
 
 export class MongoIdDto {
   @ApiProperty({
-    description: 'Identificador único (formato MongoDB) - [Referência](https://blog.eduonix.com/web-programming-tutorials/learn-structure-objectid-mongodb/)',
+    description: 'Identificador único (formato MongoDB) - [Referência](https://dev.to/shadowlik/o-que-e-o-campo-id-do-mongodb-e-como-usar-4p5d)',
   })
   @IsMongoId()
   id: string;
