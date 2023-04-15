31de000cc2336e7eb4959a567dbc97f185bec4c9
Eric Kreis <erickreis793@gmail.com>
2023-04-15 00:40:58 -0300

    update: domain pagination type

--- src/domain/types.ts
+++ src/domain/types.ts
@@ -1,8 +1,8 @@
 import { DomainSortOrder } from './enums';
 
 export type PaginationType<TDomain> = {
-  page: number;
-  limit: number;
-  sort: DomainSortOrder;
-  orderBy: keyof TDomain;
+  page?: number;
+  limit?: number;
+  sort?: DomainSortOrder;
+  orderBy?: keyof TDomain;
 };
