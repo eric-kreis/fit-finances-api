4f9c9d3a91b35092f2846e55a6b8b5c6054cca0b
Eric Kreis <erickreis793@gmail.com>
2023-04-15 16:42:49 -0300

    create: member pagination dto

---
+++ src/modules/member/dto/member-pagination.dto.ts
@@ -0,0 +1,20 @@
+import { Member } from '@domain/member/member';
+import { MemberPaginationType } from '@domain/member/member.types';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { PaginationDto } from '@shared/dtos/pagination.dto';
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class MemberPaginationDto extends PaginationDto<Member> implements MemberPaginationType {
+  @ApiPropertyOptional({
+    enum: Object.keys(Member.prototype),
+    enumName: 'MemberKeys',
+    default: 'createdAt',
+  })
+  @IsOptional()
+  @IsIn(Object.keys(Member.prototype))
+  orderBy: keyof Member = 'createdAt';
+
+  @IsOptional()
+  @IsNotEmpty()
+  serach?: string;
+}
