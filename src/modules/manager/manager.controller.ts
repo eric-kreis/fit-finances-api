4b69800f11fa0e5f745f1b64a6c89eecaf583bf1
Eric Kreis <erickreis793@gmail.com>
2023-04-14 23:45:23 -0300

    tdd: auth controller implementation

--- src/modules/manager/manager.controller.ts
+++ src/modules/manager/manager.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
-import { ManagerService } from './manager.service';
+import { Body, Controller, Param, Patch, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@auth/guards/jwt.guard';
+import { MongoIdDto } from '@shared/dtos/mongo-id.dto';
+import { ManagerService } from '@domain/manager/manager.service';
+import { ForbiddenSwagger, NotFoundSwagger, SuccessSwagger, UnauthorizedSwagger } from '@swagger/responses';
+import { Manager } from '@domain/manager/manager';
+import { createDocDescription } from '@swagger/utils';
+import { UpdateManagerDto } from './dto/update-manager.dto';
+import { ManagerEntity } from './entities/manager.entity';
 
+@ApiBearerAuth()
+@ApiTags('Gestor')
+@UseGuards(JwtAuthGuard)
 @Controller('manager')
 export class ManagerController {
-  constructor(private readonly managerService: ManagerService) {}
+  constructor(private readonly _managerService: ManagerService) {}
+
+  @ApiOperation({
+    summary: 'Atualize um gestor',
+    description: createDocDescription(
+      'Utilize este recurso para atualizar um gestor',
+      { field: 'email', constrain: 'deve ser um email com a sintaxe correta' },
+      { field: 'password', constrain: 'deve possuir 8 ou mais caracteres' },
+    ),
+  })
+  @SuccessSwagger({
+    description: 'Gestor atualizado com sucesso',
+    type: ManagerEntity,
+  })
+  @UnauthorizedSwagger()
+  @ForbiddenSwagger()
+  @NotFoundSwagger()
+  @Patch(':id')
+  public async updateManager(
+    @Param() { id }: MongoIdDto,
+    @Body() payload: UpdateManagerDto,
+    @Request() { user }: { user: Manager },
+  ): Promise<ManagerEntity> {
+    return this._managerService.update(id, payload, user.id);
+  }
 }
