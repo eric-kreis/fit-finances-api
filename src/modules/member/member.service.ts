f744c73f7f9aa545e6fe28429e3333bb3b52c283
Eric Kreis <erickreis793@gmail.com>
2023-04-15 16:55:26 -0300

    tdd: member service findById, findByCpf, findByEmail implementation

--- src/modules/member/member.service.ts
+++ src/modules/member/member.service.ts
@@ -1,7 +1,8 @@
 import { Member } from '@domain/member/member';
+import { Injectable } from '@nestjs/common';
 import { MemberRepository } from '@domain/member/member.repository';
 import { MemberService as DomainMemberService } from '@domain/member/member.service';
-import { Injectable } from '@nestjs/common';
+import { MemberNotFoundException } from '@domain/member/exceptions/member-not-found.exception';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { MemberPaginationDto } from './dto/member-pagination.dto';
@@ -23,15 +24,21 @@ export class MemberService extends DomainMemberService {
   }
 
   public async findById(id: string): Promise<Member> {
-    throw new Error('Method not implemented.');
+    const member = await this._memberRepository.findOne({ id });
+    if (!member) throw new MemberNotFoundException();
+    return member;
   }
 
   public async findByCpf(cpf: string): Promise<Member> {
-    throw new Error('Method not implemented.');
+    const member = await this._memberRepository.findOne({ cpf });
+    if (!member) throw new MemberNotFoundException();
+    return member;
   }
 
   public async findByEmail(email: string): Promise<Member> {
-    throw new Error('Method not implemented.');
+    const member = await this._memberRepository.findOne({ email });
+    if (!member) throw new MemberNotFoundException();
+    return member;
   }
 
   public async update(id: string, paylaod: UpdateMemberDto): Promise<Member> {
