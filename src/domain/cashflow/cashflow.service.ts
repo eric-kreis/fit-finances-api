c9abc8be4eebc3cfb0badb55b466063545cf5a0e
Eric Kreis <erickreis793@gmail.com>
2023-05-01 20:48:41 -0300

    update: cashflow domain

--- src/domain/cashflow/cashflow.service.ts
+++ src/domain/cashflow/cashflow.service.ts
@@ -1,10 +1,10 @@
 import { Cashflow } from './cashflow';
-import { CreateCashflowType } from './cashflow.types';
+import { CashflowPaginationType, CreateCashflowType } from './cashflow.types';
 
 export abstract class CashflowService {
   public abstract create(data: CreateCashflowType): Promise<Cashflow>;
 
-  public abstract findMany(search: any): Promise<Cashflow[]>;
+  public abstract findMany(query: CashflowPaginationType): Promise<Cashflow[]>;
 
   public abstract findById(id: string): Promise<Cashflow>;
 
--- src/domain/cashflow/cashflow.types.ts
+++ src/domain/cashflow/cashflow.types.ts
@@ -1,3 +1,10 @@
+import { PaginationType } from '@domain/types';
 import { Cashflow } from './cashflow';
+import { CashflowType } from './cashflow-type.enum';
 
 export type CreateCashflowType = Omit<Cashflow, 'id' | 'createdAt'> & { description?: string | null };
+
+export type CashflowPaginationType = PaginationType<Cashflow> & {
+  search?: string;
+  type?: CashflowType;
+};
