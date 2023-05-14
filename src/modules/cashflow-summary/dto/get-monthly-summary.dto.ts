c181b64585e8df491042c515ce89cabdb27a5832
Eric Kreis <erickreis793@gmail.com>
2023-05-14 05:46:28 -0300

    :bug: (cashflow & cashflow-summary) fix dtos

--- src/modules/cashflow-summary/dto/get-monthly-summary.dto.ts
+++ src/modules/cashflow-summary/dto/get-monthly-summary.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsNumber, IsPositive } from 'class-validator';
 
 export class GetMonthlySummariesDto {
   @ApiProperty({ description: 'Ano de competência' })
+  @Type(() => Number)
   @IsNumber()
   @IsPositive()
   year: number;
--- src/modules/cashflow/dto/cashflow-pagination.dto.ts
+++ src/modules/cashflow/dto/cashflow-pagination.dto.ts
@@ -1,18 +1,31 @@
 import { Cashflow } from '@domain/cashflow/cashflow';
 import { CashflowKeys } from '@domain/cashflow/cashflow-keys.enum';
 import { CashflowType } from '@domain/cashflow/cashflow-type.enum';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PaginationDto } from '@shared/dtos/pagination.dto';
 import { IsEnum, IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CashflowPaginationDto extends PaginationDto<Cashflow> {
+  @ApiPropertyOptional({
+    description: 'Chave de referência para ordenação',
+    enum: CashflowKeys,
+    default: CashflowKeys.createdAt,
+  })
   @IsOptional()
   @IsIn(Object.keys(CashflowKeys))
   orderBy: keyof Cashflow = 'createdAt';
 
+  @ApiPropertyOptional({
+    description: 'Campo de busca utilizado para encontrar registros a partir de suas tags ou descrição',
+  })
   @IsOptional()
   @IsNotEmpty()
   search?: string;
 
+  @ApiPropertyOptional({
+    description: 'Campo para filtrar registros de acordo com seu tipo',
+    enum: CashflowType,
+  })
   @IsOptional()
   @IsEnum(CashflowType)
   type?: CashflowType;
