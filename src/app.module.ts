54e04d2ca611f79f1cc522f87794853047132c64
Eric Kreis <erickreis793@gmail.com>
2023-05-14 05:40:12 -0300

    :sparkles: (app) updating app module :trophy:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ManagerModule } from '@modules/manager/manager.module';
+import { CashflowModule } from '@modules/cashflow/cashflow.module';
+import { CashflowSummaryModule } from '@modules/cashflow-summary/cashflow-summary.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MemberModule } from './modules/member/member.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ManagerModule,
     AuthModule,
+    ManagerModule,
+    MemberModule,
+    CashflowModule,
+    CashflowSummaryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
