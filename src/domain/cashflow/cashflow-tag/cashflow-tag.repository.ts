da6b4f580e386c3d512755081ab2cd0e5deacea8
Eric Kreis <erickreis793@gmail.com>
2023-05-24 19:42:15 -0300

    :sparkles: (cashflow) update cashflow-tag domain rep…

--- src/domain/cashflow/cashflow-tag/cashflow-tag.repository.ts
+++ src/domain/cashflow/cashflow-tag/cashflow-tag.repository.ts
@@ -3,5 +3,7 @@ import { CashflowTag } from './cashflow-tag';
 export abstract class CashflowTagRepository {
   public abstract findMany(search: string): Promise<CashflowTag[]>;
 
+  public abstract findById(id: string): Promise<CashflowTag | null>;
+
   public abstract delete(id: string): Promise<CashflowTag>;
 }
